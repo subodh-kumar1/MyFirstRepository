7c6ea472252c2002816513c6a37578b2d8f7c58b
Subodh Kumar <subodh.subodhkumar1@gmail.com>
2017-05-12 15:02:16 +0530

    Create LargestPermutation.java
    You are given an array of N integers which is a permutation of the first N natural numbers. You can swap any two elements of the array. You can make at most K swaps. What is the largest permutation, in numerical order, you can make.
    
    Input Format 
    The first line of the input contains two integers, N and K , the size of the input array and the maximum swaps you can make, respectively. The second line of the input contains a permutation of the first N natural numbers.
    
    Output Format 
    Print the lexicographically largest permutation you can make with at most K swaps.

---
+++ LargestPermutation.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class LargestPermutation {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int k = input.nextInt();
+        int[] a = new int[n];
+        int[] index = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+          a[i] = input.nextInt();
+          index[a[i]] = i;
+        }
+        for (int i = 0; i < n && k > 0; i++) {
+          if (a[i] == n - i) {
+            continue;
+          }
+          a[index[n - i]] = a[i];
+          index[a[i]] = index[n - i];
+          a[i] = n - i;
+          index[n - i] = i;
+          k--; 
+        }
+        for (int i = 0; i < n; i++) {
+          System.out.print(a[i] + " ");
+        }
+    }
+}
